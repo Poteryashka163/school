69a81b6debb737ea914ec5b4a32126ff42f02ad3
Poteryashka163 <scrat0071@gmail.com>
2024-05-03 00:43:14 +0400

    На прорверку задание 4.2(2 таблици)

--- scripts422.sql
+++ scripts422.sql
@@ -1,20 +1,17 @@
-CREATE TABLE users_cars
-(
-    user_id         INT,
-    name_person     TEXT,
-    birth_date      DATE,
-    drivers_license BOOLEAN
-);
+
 CREATE TABLE cars
 (
-    car_id    INT,
+    car_id    INT PRIMARY KEY,
     car_name  TEXT,
     car_model TEXT,
     price_car SERIAL
 );
-create TABLE ownership
+CREATE TABLE users_cars
 (
-    user_id INT,
-    car_id  INT,
-    FOREIGN KEY (user_id) REFERENCES cars (car_id)
+    user_id         INT PRIMARY KEY,
+    name_person     TEXT,
+    birth_date      DATE,
+    drivers_license BOOLEAN,
+    car_id INT,
+    FOREIGN KEY (car_id) REFERENCES cars(car_id)
 );
